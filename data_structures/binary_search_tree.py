848e8f828923af02e6e0c95d8993606fba3f59cd
Sahan Lakshitha <kksahan78@gmail.com>
2025-07-11 18:18:12 +0530

    Added: heaps and graph

--- data_structures/binary_search_tree.py
+++ data_structures/binary_search_tree.py
@@ -33,6 +33,10 @@ class BinarySearchTree:
                 temp = temp.right
         return False
 
+    """
+    Recursive Methods
+    """
+
     def contains(self, value):
         temp = self.root
         while temp:
@@ -44,6 +48,19 @@ class BinarySearchTree:
                 return True
         return False
 
+    def __r_contains(self, current_node, value):
+        if current_node == None:
+            return False
+        if value == current_node.value:
+            return True
+        if value < current_node.value:
+            return self.__r_contains(current_node.left, value)
+        if value > current_node.value:
+            return self.__r_contains(current_node.right, value)
+
+    def r_contains(self, value):
+        return self.__r_contains(self.root, value)
+
 
 
 bst = BinarySearchTree()
@@ -53,7 +70,8 @@ bst.insert(70)
 bst.insert(30)
 bst.insert(200)
 
-print(bst.root.left.left.value)
+# print(bst.root.left.left.value)
+print(bst.r_contains(200))
 # print(bst.contains(100))
 # print(bst.contains(200))
 # bst.insert(200)
---
+++ data_structures/heap.py
@@ -0,0 +1,43 @@
+
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+    def print(self):
+        print(self.heap)
+
+    def _left_child(self, index):
+        return 2 * index + 1
+
+    def _right_child(self, index):
+        return 2 * index + 2
+
+    def _parent(self, index):
+        return (index - 1) // 2
+
+    def _swap(self, index1, index2):
+        self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
+
+    def insert(self, value):
+        self.heap.append(value)
+        current = len(self.heap) - 1
+
+        while current > 0 and self.heap[current] > self.heap[self._parent(current)]:
+            self._swap(current, self._parent(current))
+            current = self._parent(current)
+
+
+
+
+
+h = MaxHeap()
+h.insert(99)
+h.insert(72)
+h.insert(61)
+h.insert(58)
+
+h.print()
+h.insert(100)
+h.print()
+
+h.insert(75)
+h.print()
